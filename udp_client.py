915c724f8e9d1d8f354edff57b92b2da1066f9ad
Bar Haran <barharan222@gmail.com>
2020-04-22 20:10:19 +0300

    Change server structure, WIP NOT WORKING

--- udp_client.py
+++ udp_client.py
@@ -1,7 +1,7 @@
 # Import the socket library
 import socket
 
-buffer_size = 1024
+BUFFER_SIZE = 1024
 
 # Get the paramters from the user
 ###### NEED TO ADD MANY CHECKS #######
@@ -21,6 +21,6 @@ udp_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 # Send to server using created UDP socket
 udp_client_socket.sendto(bytes_to_send, server_address_port)
 
-msg_from_server = udp_client_socket.recvfrom(buffer_size)
+msg_from_server = udp_client_socket.recvfrom(BUFFER_SIZE)
 msg = "Message from Server {}".format(msg_from_server[0])
 print(msg)
--- udp_server.py
+++ udp_server.py
@@ -1,31 +1,41 @@
 # Import the socket library
 import socket
 
-buffer_size  = 1024
+# Constants
+BUFFER_SIZE  = 1024
 
-# Get the paramters from the user
-###### NEED TO ADD MANY CHECKS #######
-print("What is the IP that we will listen to?")
-local_ip = input()
-print("What is the port?")
-local_port = int(input())
 
-# The DGRAM socket corresponds to the UDP
-udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Function gets the ip address from the machine, needs to be given a port manually
+def getAddressPort():
+	# Get server ip
+	server_hostname = socket.gethostname()
+	local_ip = socket.gethostbyname(server_hostname)
+	print("Server`s ip address is " + str(local_ip))
+	##### Add script argument for getting port, enforcing int ##### 
+	print("What is the port?")
+	local_port = int(input())
+	return [local_ip, local_port]
 
-# Bind to address and ip
-udp_server_socket.bind((local_ip, local_port))
 
-print("UDP server up and listening")
+def main():
+	# The DGRAM socket corresponds to the UDP
+	udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-# Listen for incoming DGRAM
-while(True):
-    bytes_address_pair = udp_server_socket.recvfrom(buffer_size)
-    message = bytes_address_pair[0]
-    address = bytes_address_pair[1]
-    client_msg = "Message from Client:{}".format(message)
-    client_ip  = "Client IP Address:{}".format(address)
-    print(client_msg)
-    print(client_ip)
-    # Sending a reply that is equal to what we received to the client
-    udp_server_socket.sendto(message, address)
+	# Get ip and port
+	local_ip, local_port = getAddressPort()
+
+	# Bind to address and ip
+	udp_server_socket.bind((local_ip, local_port))
+	print("UDP server up and listening")
+
+	# Listen for incoming DGRAM
+	while(True):
+	    bytes_address_pair = udp_server_socket.recvfrom(BUFFER_SIZE)
+	    message = bytes_address_pair[0]
+	    address = bytes_address_pair[1]
+	    client_msg = "Message from Client:{}".format(message)
+	    client_ip  = "Client IP Address:{}".format(address)
+	    print(client_msg)
+	    print(client_ip)
+	    # Sending a reply that is equal to what we received to the client
+	    udp_server_socket.sendto(message, address)
