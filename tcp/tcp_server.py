44c5aaddbf3b11de112f652126841d0c5e1a8143
Bar Haran <barharan222@gmail.com>
2020-04-23 16:47:06 +0300

    Add gitignore, fix conn

--- tcp/tcp_server.py
+++ tcp/tcp_server.py
@@ -19,15 +19,15 @@ def main():
         # Listen for incoming messages until given exit signal
         while(message != b"exit"):
                 # Recieve data from the tcp connection
-                bytes_msg = tcp_server_socket.recv(BUFFER_SIZE)
+                message = conn.recv(BUFFER_SIZE)
 
                 # Sending a reply depending on the message
                 if(message == b"quit"):
-                        tcp_server_socket.sendto(b"Goodbye", address)
+                        tcp_server_socket.sendto(b"Goodbye", addr)
                 elif (message == b"exit"):
-                        tcp_server_socket.sendto(b"Server shutting down", address)
+                        tcp_server_socket.sendto(b"Server shutting down", addr)
                 else:
-                        tcp_server_socket.sendto(b"Server got your message", address)
+                        tcp_server_socket.sendto(b"Server got your message", addr)
 
 
 if __name__ == "__main__":
